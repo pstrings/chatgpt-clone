969d73d7a1787002dfc8638731d4be88dfb5fe46
Suyash <paradisestrings99@gmail.com>
2023-03-03 01:45:17 +0530

    created sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,20 @@
+import NewChat from "./newChat";
+
+const SideBar = () => {
+  return (
+    <div className="p-2 flex flex-col h-screen">
+      <div className="flex-1">
+        <div>
+          {/* New Chat */}
+          <NewChat />
+
+          <div>{/* Model Selection */}</div>
+
+          {/* Map through the chat rows */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
